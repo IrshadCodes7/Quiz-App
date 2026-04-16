30093a7ff97122a921f45d0d74e05084fe070695
IrshadCodes7 <irshadmd92236@gmail.com>
2026-04-16 11:31:37 +0530

    add event listener to startButton to start quiz and create render quiz functionalit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded',()=>{
     //Grab Elements
+    const questionContainer = document.getElementById("question-container");
     const questionText = document.getElementById("question-text");
     const choicesList = document.getElementById("choices-list");
     const nextButton = document.getElementById("next-btn");
     const scoreDisplay = document.getElementById("score");
     const restartButton = document.getElementById("restart-btn");
     const startButton = document.getElementById("start-btn");
-    
+
+    //Fetchquestions from the API
     async function getTrivia(){
         const apiURL = "https://the-trivia-api.com/v2/questions";
         try{
@@ -25,6 +27,31 @@ document.addEventListener('DOMContentLoaded',()=>{
         }
     }
 
-    console.log(getTrivia());
+    // console.log(getTrivia());
+    let quiz;
+    startButton.addEventListener("click",async (e)=>{
+        quiz = await getTrivia();
+        console.log(quiz[0]);
+        renderQuiz(quiz[0]);
+    })
+
+    // Display quiz data
+    function renderQuiz(q){
+        questionContainer.classList.remove('hidden');
+        startButton.classList.add('hidden');
+        nextButton.classList.remove('hidden');
+        questionText.textContent = q.question.text;
+
+        for(let i = 0; i < 3; i++){
+            const li = document.createElement('li');
+            li.textContent = q.incorrectAnswers[i];
+            choicesList.appendChild(li);
+        }
+        const li = document.createElement('li');
+        li.textContent = q.correctAnswer;
+        const idx = parseInt(Math.random()*4);
+        choicesList.insertBefore(li,choicesList.children[idx]);
+    }
+
 
 })
