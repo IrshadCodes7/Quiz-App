592fa2d7cbf3372b199713a310e63ced50112ab3
IrshadCodes7 <irshadmd92236@gmail.com>
2026-04-16 15:41:04 +0530

    Add restart button functionality.Add handleEvent function that is common event handler for both startButton and restartButton.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     //Grab Elements
     const questionContainer = document.getElementById("question-container");
+    const resultContainer = document.getElementById("result-container");
     const questionText = document.getElementById("question-text");
     const choicesList = document.getElementById("choices-list");
     const nextButton = document.getElementById("next-btn");
@@ -9,49 +10,68 @@ document.addEventListener('DOMContentLoaded',()=>{
     const startButton = document.getElementById("start-btn");
 
     //Fetchquestions from the API
-    async function getTrivia(){
+    async function getTrivia() {
         const apiURL = "https://the-trivia-api.com/v2/questions";
-        try{
+        try {
             const response = await fetch(apiURL);
             // console.log(response);
-            if(!response.ok){
+            if (!response.ok) {
                 throw new Error("Failed to Fetch");
-            } 
+            }
 
-            const data  = await response.json();
+            const data = await response.json();
 
             return data;
 
-        }catch(error){
-            console.error("Error fetching question",error)
+        } catch (error) {
+            console.error("Error fetching question", error)
         }
     }
 
     // console.log(getTrivia());
     let quiz;
-    startButton.addEventListener("click",async (e)=>{
+    let quizIdx = 0;
+    async function handleEvent(){
         quiz = await getTrivia();
-        console.log(quiz[0]);
-        renderQuiz(quiz[0]);
+        console.log(quiz[quizIdx]);
+        renderQuiz(quiz[quizIdx++]);
+        resultContainer.classList.add('hidden');
+    }
+
+    startButton.addEventListener("click",handleEvent);
+    restartButton.addEventListener("click",handleEvent);
+
+    nextButton.addEventListener("click", (e) => {
+        renderQuiz(quiz[quizIdx++]);
+        if (quizIdx === 10) {
+            resultContainer.classList.remove('hidden');
+            nextButton.classList.add('hidden');
+            questionText.textContent = "";
+            choicesList.innerHTML = "";
+            quizIdx = 0;
+        }
     })
 
     // Display quiz data
-    function renderQuiz(q){
+    function renderQuiz(q) {
         questionContainer.classList.remove('hidden');
         startButton.classList.add('hidden');
         nextButton.classList.remove('hidden');
         questionText.textContent = q.question.text;
 
-        for(let i = 0; i < 3; i++){
+        choicesList.innerHTML = "";
+
+        for (let i = 0; i < 3; i++) {
             const li = document.createElement('li');
             li.textContent = q.incorrectAnswers[i];
             choicesList.appendChild(li);
         }
         const li = document.createElement('li');
         li.textContent = q.correctAnswer;
-        const idx = parseInt(Math.random()*4);
-        choicesList.insertBefore(li,choicesList.children[idx]);
+        const idx = parseInt(Math.random() * 4);
+        choicesList.insertBefore(li, choicesList.children[idx]);
     }
+
 
 
 })
